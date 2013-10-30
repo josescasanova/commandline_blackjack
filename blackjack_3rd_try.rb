bc5086673809becbe8791679fddbbbf2e69797f6
Jose Casanova <josescasanova@gmail.com>
2013-10-30 14:18:42 -0400

    Half way through 3rd run on BlackJack

--- blackjack_3rd_try.rb
+++ blackjack_3rd_try.rb
@@ -1,2 +1,97 @@
 # 3rd attempt at building blackjack game w/out instructional video
+# used help from blackjack.rb
+
+require 'pry'
+
+def calculated_total(cards)
+  card_array = cards.map { |e| e[1]}
+  total = 0
+
+  card_array.each do |x|
+    if x == '2'
+      total = total + 2
+    elsif x == '3'
+      total = total + 3
+    elsif x == '4'
+      total = total + 4
+    elsif x == '5'
+      total = total + 5
+    elsif x == '6'
+      total = total + 6
+    elsif x == '7'
+      total = total + 7
+    elsif x == '8'
+      total = total + 8
+    elsif x == '9'
+      total = total + 9
+    elsif x == '10'
+      total = total + 10
+    elsif x == 'Q'
+      total = total + 10
+    elsif x == 'K'
+      total = total + 10
+    else x == 'A'
+      total = total + 11
+    end
+  end
+  card_array.select {|a| a == "A"}.count.times do
+    if total > 21
+      total = total - 10
+    end
+  end
+  total
+end
+
+suits = ['H', 'D', 'S', 'C']
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+deck = []
+suits.each do |suit|
+  cards.each do |card|
+    deck << [suit, card]
+  end
+end
+
+deck = deck * 5
+deck.shuffle!
+
+puts '=> Welcome to BlackJack <='
+puts ''
+
+dealercards = []
+mycards = []
+
+mycards << deck.pop
+dealercards << deck.pop
+mycards << deck.pop
+dealercards << deck.pop
+
+dealertotal = calculated_total(dealercards)
+mytotal = calculated_total(mycards)
+
+puts "Dealer has #{dealercards} for a total of #{dealertotal}"
+puts "You have #{mycards} for a total of #{mytotal}"
+puts ''
+puts 'Would you like to 1) Hit or 2) Stay?'
+hit_or_stay = gets.chomp
+
+if hit_or_stay == '1'
+
+elsif hit_or_stay == '2'
+  
+else
+  puts 'You must pick 1 or 2.'
+end
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ quiz_1_answers.rb
@@ -0,0 +1,16 @@
+# 1. 1
+# 2. Array is an ordered object, like a list. A Hash contains key value pairs.
+#   ie: array = ['a', 'b', 'c']
+#   ie: hash = {name: Jose, age: 25, location: miami}
+# 3. arr is now set at [1, 2, 3, 3]
+#    returns [1, 2, 3]
+#    if you do arr.uniq!, it will now set arr to return [1, 2, 3]
+# 4. the .map method gets each object and runs a block for each one.
+#     the .select method selects certain objects dependant on the criteria stated
+# 5. You would set each key value using strings.
+# => ie: hash = { "a" => "1", "c" => "2" }
+# 6. 
+# 7. 
+# 8. 
+# 9. 
+# 10.
